4cd4bea5f8709b50dcac08d0e5ebc96ae49d120c
Venky <venkatramanm@gmail.com>
2025-01-02 01:02:35 +0530

    1024 for did size

--- src/main/java/in/succinct/defs/db/model/did/identifier/Did.java
+++ src/main/java/in/succinct/defs/db/model/did/identifier/Did.java
@@ -1,10 +1,12 @@
 package in.succinct.defs.db.model.did.identifier;
 
+import com.venky.swf.db.annotations.column.COLUMN_SIZE;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 
 public interface Did {
     
     @UNIQUE_KEY
+    @COLUMN_SIZE(1024)
     String getDid();
     void setDid(String did);
     
